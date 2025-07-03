f26b1d6e07de603a90b8f3033b184f109b878213
zyc124578 <yc-zhao22@mails.tsinghua.edu.cn>
2025-07-03 14:47:15 +0800

    Create config.py

---
+++ backend_local/app/config.py
@@ -0,0 +1,5 @@
+# app/config.py
+# 百度千帆 API 配置
+BAIDU_API_URL = "https://qianfan.baidubce.com/v2/app/conversation"
+BAIDU_APP_ID = "f7424d75-3523-454c-87af-d50f455729c6"  # 你的 app_id
+BAIDU_AUTH_TOKEN = "Bearer bce-v3/ALTAK-acJ0DM86Ly28qeK89yA74/f5ec5af120f5d8efc28948e54314b33205595368"  # 替换成实际的 Authorization 值
